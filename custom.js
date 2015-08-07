7a4fc2d94df2e4ff969a68bd31c1bb9407194624
luis <ferrerluis97@gmail.com>
2015-08-07 14:46:46 -0400

    renamed files and modified custom.js

--- custom.js
+++ custom.js
@@ -4,13 +4,13 @@ myApp.config(['$routeProvider', function ($routeProvider) {
 	
 	$routeProvider
 		.when('/events', {
-			templateUrl: 'https://raw.githubusercontent.com/PantherHackers/recruiting/gh-pages/events.html',
+			templateUrl: './events.html',
 			controller: 'EventsController'
 		})
-		// .when('/', {
-		// 	templateUrl: 'home.html',
-		// 	controller: 'HomeController'
-		// })
+		.when('/', {
+			templateUrl: './home.html',
+			controller: 'HomeController'
+		})
 		.otherwise({
 			redirectTo: '/'
 		});
