a584fd4ae0577c5a909fa9ab09dc174b55f5d763
Caleb Lewis <caleb_dre@yahoo.com>
2015-08-12 20:41:38 -0400

    fixed up the events page and form

--- www/wordpress-default/wp-content/themes/pantherhackers/functions.php
+++ www/wordpress-default/wp-content/themes/pantherhackers/functions.php
@@ -39,33 +39,38 @@ function event_meta_box_content($post){
 	wp_nonce_field( plugin_basename( __FILE__ ), 'events_info_box_content_nonce' );
 	
 	// couldn't we create a 'templates' folder, extract the html to a file there, and then import the file here?
+	echo '<label for="event_month">Month:</label> ';
+	echo '<input type="text" id="event_month" name="event_month" placeholder="ex: February" value="'.get_post_meta($post->ID, "month", true).'" />';
+	
+	echo '<br /><br />';
+	
 	echo '<label for="event_date">Date:</label> ';
-	echo '<input type="text" id="event_date" name="event_date" placeholder="enter a date" value="'.get_post_meta($post->ID, "date", true).'" />';
+	echo '<input type="text" id="event_date" name="event_date" placeholder="ex: 22" value="'.get_post_meta($post->ID, "date", true).'" />';
 	
 	echo '<br /><br />';
 	
 	echo '<label for="event_start_time">Start Time: </label> ';
-	echo '<input type="text" id="event_start_time" name="event_start_time" placeholder="enter a start time" value="'.get_post_meta($post->ID, 'start_time', true).'"/>';
+	echo '<input type="text" id="event_start_time" name="event_start_time" placeholder="ex: 11:00 am" value="'.get_post_meta($post->ID, 'start_time', true).'"/>';
 	
 	echo '<br /><br />';
 	
 	echo '<label for="event_end_time">End Time: </label> ';
-	echo '<input type="text" id="event_end_time" name="event_end_time" placeholder="enter an end time" value="'.get_post_meta($post->ID, 'end_time', true).'"/>';
+	echo '<input type="text" id="event_end_time" name="event_end_time" placeholder="12:30 pm" value="'.get_post_meta($post->ID, 'end_time', true).'"/>';
 	
 	echo '<br /><br />';
 	
 	echo '<label for="event_location">Location: </label> ';
-	echo '<input type="text" id="event_location" name="event_location" placeholder="enter a location" value="'.get_post_meta($post->ID, 'location', true).'"/>';
+	echo '<input type="text" id="event_location" name="event_location" placeholder="1234 Awesome Street. Atlanta Ga. 30303" value="'.get_post_meta($post->ID, 'location', true).'"/>';
 	
 }
 
 function event_info_box_save( $post_id ) {
-	
-	if($_POST){
+	// if the first isn't set, why would the others be?	
+	if($_POST && isset($_POST['event_month'])){
 		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
 		return;
 		
-		if ( !wp_verify_nonce( $_POST['events_info_box_content_nonce'], plugin_basename( __FILE__ ) ) )
+		if (isset($_POST['events_info_box_content_nonce']) && !wp_verify_nonce( $_POST['events_info_box_content_nonce'], plugin_basename( __FILE__ ) ) )
 		return;
 		
 		if ( 'page' == $_POST['post_type'] ) {
@@ -76,10 +81,11 @@ function event_info_box_save( $post_id ) {
 			return;
 		}
 		
+		update_post_meta( $post_id,'month', $_POST['event_month']);
 		update_post_meta( $post_id,'date', $_POST['event_date']);
 		update_post_meta( $post_id,'start_time', $_POST['event_start_time']);
 		update_post_meta( $post_id,'end_time', $_POST['event_end_time']);
-		update_post_meta( $post_id,'location', $_POST['event_location']);
+		update_post_meta( $post_id,'location', $_POST['event_location']);	
 	}
 		
 }
